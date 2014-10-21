99210db124622ae9f992138d8ae82a6e678dcd8e
lbarsov <lbarsov@users.noreply.github.com>
2014-10-21 03:39:11 +0300

    Create en_EN.php

---
+++ i18n_link_manager/lang/en_EN.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Link Manager English language file by Rogier Koppejan
+ */
+
+
+$i18n = array(
+
+"PLUGIN_NAME"         =>  "Link Manager",
+"NO_LINKS"            =>  "No links have been defined yet. You can add links by clicking on the <i>\"Add New Link\"</i> button in the top right corner of this screen.",
+"CHANGE_ORDER"        =>  "Change the order of the links below using drag &amp; drop. You can save your changes by clicking on the <i>\"Save Order\"</i> button at the bottom of this screen.",
+"ERROR_SAVE"          =>  "<b>Error:</b> Unable to save the link. <em>CHMOD 777</em> the folder /data/other and its contents and retry.",
+"ERROR_DELETE"        =>  "<b>Error:</b> Unable to delete the link. <em>CHMOD 777</em> the folder /data/other and its contents and retry.",
+"ERROR_RESTORE"       =>  "Unable to undo your changes.",
+"SUCCESS_SAVE"        =>  "Your changes have been saved.",
+"SUCCESS_DELETE"      =>  "The link has been deleted.",
+"SUCCESS_RESTORE"     =>  "Your changes have been undone.",
+"NEW_LINK"            =>  "Add New Link",
+"NAME"                =>  "Name",
+"DESCRIPTION"         =>  "Description",
+"URL"                 =>  "URL",
+"EDIT_LINK"           =>  "Edit Link",
+"VIEW_LINK"           =>  "View Link",
+"DELETE_LINK"         =>  "Delete Link",
+"SAVE_LINK"           =>  "Save Link",
+"SAVE_ORDER"          =>  "Save Order",
+"CANCEL"              =>  "Cancel",
+"DELETE"              =>  "Delete",
+"OR"                  =>  "or",
+
+);
+
+?>
